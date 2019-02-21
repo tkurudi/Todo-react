3c71c390cfe39984cee93012fd8aa4ef84391c29
tkurudi <t.kurudi209@gmail.com>
2019-02-21 13:53:40 +1300

    test

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import Todos from './components/Todos';
+import Header from './components/layout/Header'
+
+import './App.css';
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Take out the trash',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Dinner with wife',
+        completed: true
+      },
+      {
+        id: 3,
+        title: 'Meeting with boss',
+        completed: false
+      }
+    ]
+  }
+  markComplete = (id) => {
+    this.setState({todos: this.state.todos.map(todo => {
+      if(todo.id === id) {
+        todo.completed = !todo.completed
+      }
+      return todo;
+    })})
+}
+
+delTodo = (id) => {
+  this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)]})
+}
+
+  render() {
+    return (
+      <div className="App">
+        {/* <Header/> */}
+        <Todos todos={this.state.todos} markComplete={this.markComplete}
+        delTodo={this.delTodo}/>
+      </div>
+    );
+  }
+}
+
+export default App;
